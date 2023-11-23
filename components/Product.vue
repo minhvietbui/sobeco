5b83b6ba1fe0f5037af4f75102250c597f9aa008
VietBM <vietbm9@gmail.com>
2023-11-23 15:49:06 +0700

    Fixed Width of the picture

--- components/Product.vue
+++ components/Product.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="w-screen flex items-center justify-center">
-    <NuxtImg src="/images/Sec1.jpg" loading="lazy" format="webp" fit="fill" />
+    <NuxtImg
+      src="/images/Sec1.jpg"
+      loading="lazy"
+      format="webp"
+      fit="fill"
+      width="1440"
+      height="600"
+    />
   </div>
 </template>
